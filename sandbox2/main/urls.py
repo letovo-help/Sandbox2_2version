f1a00e28afb1b590e336425f2afc8efa337a795b
KuznetsovaViktoria <Kuznetzowa.viktoria@gmail.com>
2020-12-12 09:51:20 +0300

    new pages, editing requests, date of publishing

--- sandbox2/main/urls.py
+++ sandbox2/main/urls.py
@@ -5,7 +5,16 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='all requests'),
-    path('about', views.about, name='about us'),
+    path('<int:pk>', views.ReqsDetailView.as_view(), name='request details'),
+    path('<int:pk>/update', views.ReqsUpdateView.as_view(), name='request update'),
     path('new_request', views.new_request, name='new request'),
+    path('my_requests', views.my_requests, name='my requests'),
     path('successful_new_request', views.successful_new_request, name='successful new request'),
+    path('about', views.about, name='about us'),
+path('rules', views.rules, name='rules'),
+path('contacts', views.contacts, name='contacts'),
+path('ofc_info', views.ofc_info, name='ofc info'),
+path('help', views.help, name='help'),
+path('feedback', views.feedback, name='feedback'),
+path('personal_account', views.personal_account, name='personal account'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- sandbox2/main/views.py
+++ sandbox2/main/views.py
@@ -1,14 +1,26 @@
 from django.shortcuts import render, redirect
 from .models import Requests
 from .forms import RequestsForm
-def index(request):
-    return render(request, 'main/index.html')
+from .models import Requests
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView, UpdateView
 
-def about(request):
-    return render(request, 'main/about.html')
+class ReqsDetailView(DetailView):
+    model = Requests
+    template_name = 'main/detail_view.html'
+    context_object_name = 'request'
+
+class ReqsUpdateView(UpdateView):
+    model = Requests
+    template_name = 'main/request_update.html'
+    form_class = RequestsForm
+
+
+def index(request):
+    req = Requests.objects.all()
+    return render(request, 'main/index.html', {'req' : req})
 
 def new_request(request):
-    #req = Requests.objects.all()
     error = ''
     if request.method == 'POST':
         form = RequestsForm(request.POST)
@@ -28,3 +40,22 @@ def new_request(request):
 
 def successful_new_request(request):
     return render(request, 'main/successful_new_req.html')
+
+def my_requests(request):
+    req = Requests.objects.all()
+    return render(request, 'main/my_requests.html', {'req': req, 'order': [1, 1, 1, 0, 0], 'o': 3, 'do': [1, 1, 0, 0, 0], 'd': 2})
+
+def about(request):
+    return render(request, 'main/about.html')
+def rules(request):
+    return render(request, 'main/rules.html')
+def contacts(request):
+    return render(request, 'main/contacts.html')
+def ofc_info(request):
+    return render(request, 'main/ofc_info.html')
+def help(request):
+    return render(request, 'main/help.html')
+def feedback(request):
+    return render(request, 'main/feedback.html')
+def personal_account(request):
+    return render(request, 'main/personal_account.html', {'order': [1, 1, 1, 0, 0], 'o': 3, 'do': [1, 1, 0, 0, 0], 'd': 2})
